b4cd2888d80b7417b0ddb0750d4bebb0044c6ada
Owen-Garber <115043214+Owen-Garber@users.noreply.github.com>
2022-10-05 13:16:49 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from Inputs import Inputs
+from Dyer_Model import Dyer_Model
+
+def main():
+    Inputs = Inputs()
+    hole_numbers=Dyer_Model(Inputs)
+    circumference_hole_ring_1 =
+    circumference_hole_ring_2 =
+    circumference_hole_ring_3 =
+    number_holes_ring_1 =
+    number_holes_ring_2 =
+    number_holes_ring_3 =
