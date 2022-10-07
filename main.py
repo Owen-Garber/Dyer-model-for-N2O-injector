030338800b56d6afecb09324df45c5e36125f363
Owen-Garber <115043214+Owen-Garber@users.noreply.github.com>
2022-10-07 16:57:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from Dyer_Model import Dyer_Model
 def main():
     Inputs = Inputs()
     hole_numbers=Dyer_Model(Inputs)
+    outer_diameter_of_oriface_plate
     circumference_hole_ring_1 =
     circumference_hole_ring_2 =
     circumference_hole_ring_3 =
+    spacing_between_holes
+    spacing_between_rings
     number_holes_ring_1 =
     number_holes_ring_2 =
     number_holes_ring_3 =
+    angle_min_first_ring = 
+    subsequent_jump_in_angle = 
+    
