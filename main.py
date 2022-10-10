0e3a7d7e5a5a30d37bb4b5c98439632f98139187
Owen-Garber <115043214+Owen-Garber@users.noreply.github.com>
2022-10-10 12:26:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print("********************hole geometry************************")
 
 hole_number=12 #from dyer model
 D_2=.00157 #[m] from RPE
+thickness_of_orifice_plate=.00635 #[m] = .25 inches
 countersink_max_diameter=1.2*D_2 #from nasa injector reference pg 38. assumed to be half as effective as a rounded entrance radius, 1.2 maximizes Cd.
 print(hole_number, "holes needed of size", D_2, "[m] with a countersink at the entrance")
 outer_diameter_of_orifice_plate=0.0381 #[m] = 1.5 [in]
@@ -19,11 +20,12 @@ print("the number of holes in the most inner ring of holes will be ", number_hol
 print("thus, there will be two times as many holes in the 2nd most inner ring of holes to keep enough spacing")
 number_holes_ring_2=8
 print("the number of holes in the most inner ring of holes will be ", number_holes_ring_2)
-angle_first_ring = 60 #degrees, produces ideal characteristics, from https://ntrs.nasa.gov/citations/19760023196, 
+angle_first_ring = 70 #degrees, 60 produces ideal characteristics, from https://ntrs.nasa.gov/citations/19760023196,
 print("the angle of the first hole ring will be ", angle_first_ring, "degrees from the surface of the orifice plate")
-impingement_point_y_coord=(diameter_hole_ring_1/2)*math.tan(angle_first_ring) #want to be around 5-7 * D_2
+impingement_point_y_coord=(diameter_hole_ring_1/2)*math.tan(math.radians(angle_first_ring)) #want to be around 5-7 * D_2
 ideal_impingement_point_y_coord = 6*D_2*math.sin(math.radians(angle_first_ring)) # from https://ntrs.nasa.gov/citations/19760023196 pg 29. 
 print("ideal impingement point is ", ideal_impingement_point_y_coord, "below the bottom of the orifice plate")
+print("which is the y component of an angled jet ")
 print("the holes will impinge at ", impingement_point_y_coord, "[m] from the **top** of the orifice plate")
 angle_second_ring = math.atan(impingement_point_y_coord/(diameter_hole_ring_2/2)) #radians, just using geometry
 angle_second_ring=math.degrees(angle_second_ring) #converted to degrees
@@ -32,9 +34,5 @@ print("so that the two rings of holes impinge at the same point below the orific
 
 
 print("********************diffuser geometry************************")
-max_angle_diffuser=10 #degrees, from page 48 of liquid rocket engine injectors by NASA. NASA says no greater than 10 degrees, being safe. https://ntrs.nasa.gov/citations/19760023196
-ID_inlet_injector= .00635 #[m] from CAD
-height_of_diffuser=(outer_diameter_of_orifice_plate-ID_inlet_injector)/(math.degrees(math.tan(max_angle_diffuser)))
-print("the required height from bottom of inlet to top of orifice plate at ", max_angle_diffuser, "degrees from vertical is ", height_of_diffuser, "[m]")
-print(outer_diameter_of_orifice_plate-ID_inlet_injector)
-print(math.degrees(math.tan(max_angle_diffuser)))
+max_angle_diffuser=10 #degrees, from page 48 of liquid rocket engine injectors by NASA. NASA says no greater than 15 degrees, being safe. https://ntrs.nasa.gov/citations/19760023196
+print("the max angle of the diffuser is ", max_angle_diffuser, "degrees from vertical plane")
